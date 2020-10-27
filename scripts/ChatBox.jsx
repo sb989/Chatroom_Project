f5927bdcd95bd83808f1613115681dcef426b598
sb989 <sb989@njit.edu>
2020-10-27 01:24:42 +0000

    commit 1 of cleaning javascript

--- scripts/ChatBox.jsx
+++ scripts/ChatBox.jsx
@@ -1,138 +1,125 @@
 import * as React from 'react';
 import { Socket } from './Socket';
 import { Send } from './Send';
-import {Messages} from './Messages';
-export function ChatBox(params)
-{
-    var name = params['name'];
-    var messages = params['messages'];
-    var setMessages = params['setMessages'];
-    var email = params['email'];
-    
-    function firstConnect()
-    {
-        React.useEffect(()=>
-        {
-            Socket.on('connected',(data)=>
-            {
-                console.log("connected");
-                var messages = data['messages'];
-                var size = messages.length;
-                var i;
-                var message,text,sender_name,dt,msg_type;
-                var sender_email,img,same_or_diff_sender;
-                setMessages([]);
-                var mess = [];
-                for(i=0;i<size;i++)
-                {
-                    message = messages[i];
-                    msg_type = message['msg_type']
-                    text = message['msg'];
-                    sender_name = message['name'];
-                    sender_email = message['email'];
-                    dt = message['dt'];
-                    img = message['img'];
-                    same_or_diff_sender = message["same_or_diff_sender"];
-                    mess[i] = createMessage(text,dt,sender_name,msg_type,sender_email,img,same_or_diff_sender);
-                }
-                setMessages(m=>m.concat(mess));
-            });
-            var element = document.getElementById(0);
-            if(element)
-                element.scrollIntoView(false);
-            
-            
-        },[]);
+import { Messages } from './Messages';
+
+export default function ChatBox(params) {
+  const { name } = params;
+  const { messages } = params;
+  const { setMessages } = params;
+  const { email } = params;
+
+  function createMessage(text, dt, s_name, msg_type, s_email, img, same_or_diff_sender) {
+    if (same_or_diff_sender === '' && messages.length > 0 && messages[messages.length - 1].email === s_email) {
+      same_or_diff_sender = 'same_sender';
+    } else if (same_or_diff_sender === '') {
+      same_or_diff_sender = 'diff_sender';
     }
-    
-    function createMessage(text,dt,s_name,msg_type,s_email,img,same_or_diff_sender)
-    {
-        if(same_or_diff_sender === '' && messages.length > 0 && messages[messages.length-1]['email'] === s_email)
-        {
-            same_or_diff_sender = "same_sender";
+    const message = {
+      dt,
+      name: s_name,
+      text,
+      msg_type,
+      email: s_email,
+      img,
+      same_or_diff_sender,
+    };
+    return message;
+  }
+
+  function addMessage(text, dt, sName, msgType, sEmail, img, sameOrDiffSender) {
+    const message = createMessage(text, dt, sName, msgType, sEmail, img, sameOrDiffSender);
+    setMessages((m) => m.concat(message));
+  }
+
+  function firstConnect() {
+    React.useEffect(() => {
+      Socket.on('connected', (data) => {
+        const mssgs = data.messages;
+        const size = mssgs.length;
+        let i;
+        let message; let text; let senderName; let dt; let msgType;
+        let senderEmail; let img; let sameOrDiffSender;
+        setMessages([]);
+        const mess = [];
+        for (i = 0; i < size; i += 1) {
+          message = mssgs[i];
+          msgType = message.msg_type;
+          text = message.msg;
+          senderName = message.name;
+          senderEmail = message.email;
+          dt = message.dt;
+          img = message.img;
+          sameOrDiffSender = message.same_or_diff_sender;
+          mess[i] = createMessage(
+            text,
+            dt,
+            senderName,
+            msgType,
+            senderEmail,
+            img,
+            sameOrDiffSender,
+          );
         }
-        else if (same_or_diff_sender === '')
-        {
-            same_or_diff_sender = "diff_sender";
+        setMessages((m) => m.concat(mess));
+      });
+      const element = document.getElementById(0);
+      if (element) element.scrollIntoView(false);
+    }, []);
+  }
+
+  function receiveMessage() {
+    React.useEffect(() => {
+      Socket.on('new message', (data) => {
+        if (name === null) return;
+
+        if (data.email === email) {
+          if (data.index !== -1) {
+            const copy = [...messages];
+            copy[data.index].msg_type = data.msg_type;
+            setMessages(() => copy);
+          }
+
+          return;
         }
-        var message ={'dt':dt,'name':s_name,
-            'text':text,'msg_type':msg_type,
-            'email':s_email,'img':img,
-            "same_or_diff_sender":same_or_diff_sender};
-        return message
-    }
-    function addMessage(text,dt,s_name,msg_type,s_email,img,same_or_diff_sender)
-    {
-        var message = createMessage(text,dt,s_name,msg_type,s_email,img,same_or_diff_sender);
-        setMessages(m=>m.concat(message));
-    }
-    
-   
-    
-    function receiveMessage()
-    {
-        React.useEffect(()=>
-        {
-            Socket.on('new message',(data)=>
-            {
-                if(name===null)    
-                    return;
-                    
-                if(data['email'] === email)
-                {
-                    if(data['index'] !=-1)
-                    {
-                        var copy = [...messages]
-                        copy[data['index']]['msg_type'] = data['msg_type'];
-                        setMessages(m=>copy); 
-                    }
-                        
-                    return;
-                }
-                addMessage(data['msg'],data['dt'],data['name'],data['msg_type'],data['email'],data['img'],'');
-                
-            });
-            return ()=>
-            {
-                Socket.removeEventListener('new message');
-            }
-        });
-    }
-    
-    function receiveBotMessage()
-    {
-        React.useEffect(()=>
-        {
-            Socket.on('Bot',(data)=>
-            {
-                addMessage(data['msg'],data['dt'],data['name'],data['msg_type'],data['email'],data['img'],'');
-            });
-        
-            return ()=>
-            {
-                Socket.removeEventListener('Bot');
-            }
-        });
-    }
-  
-    
-    firstConnect();
-    receiveMessage();
-   receiveBotMessage();
-    return(
-        <div className = "chatBox" id ="chatBox">
-           
-            <Messages
-                messages = {params['messages']}
-                email = {email}
-            />
-            
-            <Send name={params['name']} 
-            messages={params['messages']}
-            addMessage={addMessage}
-            email={email}
-            img = {params['img']}    
-            />
-        </div>
-        )
+        addMessage(data.msg, data.dt, data.name, data.msg_type, data.email, data.img, '');
+      });
+      return () => {
+        Socket.removeEventListener('new message');
+      };
+    });
+  }
+
+  function receiveBotMessage() {
+    React.useEffect(() => {
+      Socket.on('Bot', (data) => {
+        addMessage(data.msg, data.dt, data.name, data.msg_type, data.email, data.img, '');
+      });
+
+      return () => {
+        Socket.removeEventListener('Bot');
+      };
+    });
+  }
+
+  firstConnect();
+  receiveMessage();
+  receiveBotMessage();
+  return (
+    <div className="chatBox" id="chatBox">
+
+      <Messages
+        messages={params.messages}
+        email={email}
+      />
+
+      <Send
+        name={params.name}
+        messages={params.messages}
+        addMessage={addMessage}
+        email={email}
+        img={params.img}
+      />
+    </div>
+  );
 }
--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,78 +1,76 @@
 import * as React from 'react';
 import { Socket } from './Socket';
-import {ChatBox} from './ChatBox';
-import {GoogleButton} from './GoogleButton';
-export function Content() {
-    
-    const[messages,setMessages] = React.useState([]);
-    const[name,setName] = React.useState(null);
-    const[email,setEmail] = React.useState('');
-    const[roomCount,setRoomCount] = React.useState(0);
-    const[authenticated,setAuthenticated] = React.useState(false);
-    const[loginMessage,setLoginMessage] = React.useState('');
-    const[img,setImg] = React.useState('');
-    
-        
-    function receiveCount()
-    {
-        React.useEffect(()=>{
-            Socket.on('room_count',(data)=>{
-                setRoomCount(data['count']);
-                console.log('receiveCount');
-            })
-            return ()=>{Socket.removeEventListener('room_count');}
-        });
-    }
-    
-    function disconnect()
-    {
-        
-        React.useEffect(()=>{
-            Socket.on('disconnect',()=>{
-                alert("You are not connected to the server. Messages might send when you reconnect.");    
-            })
-            return ()=>{Socket.removeEventListener('disconnect');}
-        });
-    }
-    
-    
-    receiveCount();
-    disconnect();
-    
-    if(authenticated)
-    {
-        return(
-                <div>
-                    <div className = "top">
-                        <h1 className="Chat">CHAT</h1>
-                        <h2 className="roomCount">Room Count: {roomCount}</h2>
-                    </div>
-                    <ChatBox 
-                    name = {name} setName = {setName} 
-                    messages = {messages} img = {img}
-                    setMessages = {setMessages}
-                    email = {email}
-                    />
-                </div>
-                
-                );    
-        
-    }
-        
-    else if(! authenticated)
-    {
-        return (<div className = "loginBox">
-                <h1 className = "loginHeader">Login</h1>
-                {loginMessage}<br/>
-                <GoogleButton 
-                className = "googleButton"
-                setAuthenticated = {setAuthenticated} 
-                setLoginMessage = {setLoginMessage}
-                setName = {setName}
-                setImg = {setImg}
-                setEmail = {setEmail}
-                />
-            </div>);
-    }
-    
+import ChatBox from './ChatBox';
+import { GoogleButton } from './GoogleButton';
+
+export default function Content() {
+  const [messages, setMessages] = React.useState([]);
+  const [name, setName] = React.useState(null);
+  const [email, setEmail] = React.useState('');
+  const [roomCount, setRoomCount] = React.useState(0);
+  const [authenticated, setAuthenticated] = React.useState(false);
+  const [loginMessage, setLoginMessage] = React.useState('');
+  const [img, setImg] = React.useState('');
+
+  function receiveCount() {
+    React.useEffect(() => {
+      Socket.on('room_count', (data) => {
+        setRoomCount(data.count);
+      });
+      return () => { Socket.removeEventListener('room_count'); };
+    });
+  }
+
+  function disconnect() {
+    React.useEffect(() => {
+      Socket.on('disconnect', () => {
+        alert('You are not connected to the server. Messages might send when you reconnect.');
+      });
+      return () => { Socket.removeEventListener('disconnect'); };
+    });
+  }
+
+  receiveCount();
+  disconnect();
+
+  if (authenticated) {
+    return (
+      <div>
+        <div className="top">
+          <h1 className="Chat">CHAT</h1>
+          <h2 className="roomCount">
+            Room Count:
+            {roomCount}
+          </h2>
+        </div>
+        <ChatBox
+          name={name}
+          setName={setName}
+          messages={messages}
+          img={img}
+          setMessages={setMessages}
+          email={email}
+        />
+      </div>
+
+    );
+  }
+
+  if (!authenticated) {
+    return (
+      <div className="loginBox">
+        <h1 className="loginHeader">Login</h1>
+        {loginMessage}
+        <br />
+        <GoogleButton
+          className="googleButton"
+          setAuthenticated={setAuthenticated}
+          setLoginMessage={setLoginMessage}
+          setName={setName}
+          setImg={setImg}
+          setEmail={setEmail}
+        />
+      </div>
+    );
+  }
 }
